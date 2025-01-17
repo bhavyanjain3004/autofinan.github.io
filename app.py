d63e1cda3b43e230771dee35fe1d938bf880a85c
Bhavya jain <bhavyajain3004@gmail.com>
2025-01-18 04:10:45 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,41 +2,47 @@ from openai import OpenAI
 import streamlit as st
 import instructor
 from pydantic import BaseModel
+from stock_data import get_stock_prices
 
 
-
-
-client = OpenAI(api_key="dhddjcnd", base_url="http://localhost:8000/v1")
+client = OpenAI(api_key="jhjhjh1234", base_url="http://localhost:8000/v1")
 client = instructor.patch(client=client)
 
 class ResponseModel(BaseModel):
-   ticker:str
-   days:int
+    ticker: str
+    days: int
 
-st.title("🚀 Fake OpenAI Server App (...llama cpp)")
+st.title(" OpenAI Server App (...llama cpp)")
 prompt = st.chat_input("Pass your prompt here")
 
 if prompt:
-   st.chat_message("user").markdown(prompt)
+    st.chat_message("user").markdown(prompt)
 
-response = client.chat.completions.create(
-    model="models/mistral-7b-instruct-v0.1.Q4_0.gguf",
-    messages=[{"role": "user","content": prompt
-    }],
-    #stream=True,
-    response_model=ResponseModel, 
-)
+    response = client.chat.completions.create(
+        model="mistral-function-calling",
+        messages=[{"role": "user", "content": prompt}],
+        
+        response_model=ResponseModel,
+    )
 
-st.chat_message('ai').markdown(response)
+    st.chat_message("ai").markdown(response)
 
-#with st.chat_message('ai'):
-   #completed_message = ""
-   #message = st.empty()
-#for chunk in response:
-   #if chunk.choices[0].delta.content is not None:
-      #completed_message += chunk.choices[0].delta.content
-      #message.markdown(completed_message)
-   #print(chunk.choices[0].delta.content, flush=True, end="")
+    try:
+        prices = get_stock_prices(response.ticker, response.days)
+        st.chat_message("ai").markdown(prices)
+        fullresponse = client.chat.completions.create(
+            model="mixtral",
+            messages=[{"role": "user", "content": prompt + "\n" + str(prices)}],
+            stream=True,
+        )
 
+        with st.chat_message("ai"):
+            completed_message = ""
+            message = st.empty()
+            for chunk in fullresponse:
+                if chunk.choices[0].delta.content is not None:
+                    completed_message += chunk.choices[0].delta.content
+                    message.markdown(completed_message)
+    except Exception as e:
+        st.chat_message("ai").markdown("Something went wrong 😭")
 
-#print(response.choices[0].message.content)
